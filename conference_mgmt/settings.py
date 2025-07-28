e7d88d10d7a5bb255665fa37935292d6c0a2280b
HemangJangir <hemangjangir@gmail.com>
2025-07-28 11:22:51 +0530

    passward

--- conference_mgmt/settings.py
+++ conference_mgmt/settings.py
@@ -110,6 +110,13 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+# Custom authentication backend for email/username login
+AUTHENTICATION_BACKENDS = [
+    'accounts.backends.EmailOrUsernameModelBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
